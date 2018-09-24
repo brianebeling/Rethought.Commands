1f648be8e7d662202a3731536b74cae69fb5cb25
Brian Ebeling <bje@live.de>
2018-09-24 18:55:24 +0200

    AsyncTypeParser#Aborted is no longer async

--- Rethought.Commands/Parser/AsyncTypeParser.cs
+++ Rethought.Commands/Parser/AsyncTypeParser.cs
@@ -8,9 +8,6 @@ namespace Rethought.Commands.Parser
     {
         public abstract Task<Option<Option<TOutput>>> ParseAsync(TInput input, CancellationToken cancellationToken);
 
-        protected Task<Option<Option<TOutput>>> Aborted()
-        {
-            return Task.FromResult(default(Option<Option<TOutput>>));
-        }
+        protected Option<Option<TOutput>> Aborted() => default;
     }
 }
